f6f0834493905ab0eca9cdacda50b016d1601c86
Avinash Rath <avinash.rrath@gmail.com>
2021-09-23 16:50:01 +0530

    Created Happy Birthday API

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+
+//--------------REQUESTS--------------//
+/**
+ * Takes a single parameter `name`, returns a string in response.
+ * the response String is `Happy Birthday, <name>!`
+ * e.g. name = "Elon Musk"
+ *      response = "Happy Birthday, Elon Musk!"
+ */
+app.post('/sendName', (req, res) => {
+    // getting the name parameter.
+    const name = req.query.name;
+    if(name == null) {
+        // checking if the request is valid or not.
+        console.log("Invalid request, name parameter was empty"); // log
+        res.statusCode = 400; // bad request
+        res.statusMessage = "Name parameter was empty. You must provide a name";
+        res.send();
+    } else {
+        res.send("Happy Birthday, " + name +"!");
+    }
+});
+
+
+app.listen(port, () => console.log("happy birthday app running at port 8080"));
